30a776ca24a475bd8dacb57c2efdac2520d636d2
joan <jenietor0311@gmail.com>
2023-07-13 23:05:39 -0500

    add settings.go and file configuration

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+
+	"github.com/Jenietor2/app-inventory/settings"
+	"go.uber.org/fx"
+)
+
+func main() {
+	app := fx.New(
+		fx.Provide(
+			settings.New,
+		),
+		fx.Invoke(
+			func(s *settings.Settings) {
+				log.Println(s)
+			},
+		),
+	)
+
+	app.Run()
 }
